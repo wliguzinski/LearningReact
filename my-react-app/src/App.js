f7bce48a3d65663c3412bc21921a8f2da780662c
Wojciech Liguzinski <wojciech.liguzinski@mobica.com>
2021-01-22 11:42:43 +0100

    Added a draft hamburger menu.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { Burger, Menu } from './components';
+import { useOnClickOutside } from './hooks';
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +31,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ my-react-app/src/My.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class My extends Component {
+  render() {
+    return (
+        <p>
+          Mój tekst
+        </p>
+    );
+  }
+}
+ 
+export default My;
